d6eedcd61c2b71fe12ab693bea0ace28ae908371
Umut Çelikkol <umutcelikkol@yahoo.com.tr>
2023-06-24 17:56:41 +0300

    array metodlari eklendi

---
+++ Pratikler/diziler-array-sinif-metodlari/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace diziler_array_sinif_metodlari;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+       //Sort Metodu
+       Console.WriteLine("****** Sirasiz Dizi ******");
+       foreach (var sayi in sayiDizisi)
+       {
+           Console.WriteLine(sayi);
+       }
+       Console.WriteLine("****** Sirali Dizi ******");
+       Array.Sort(sayiDizisi);
+       foreach (var sayi in sayiDizisi)
+       {
+           Console.WriteLine(sayi);
+       }
+       //Clear Metodu
+        Console.WriteLine("****** Array Clear ******");
+        Array.Clear(sayiDizisi,2,2); // 2.indexten baslayarak 2 elemani sifirlar.
+        foreach (var sayi in sayiDizisi)
+        {
+           Console.WriteLine(sayi);
+        }
+        //Reverse Metodu
+        Console.WriteLine("****** Array Reverse ******");
+        Array.Reverse(sayiDizisi); 
+        foreach (var sayi in sayiDizisi)
+        {
+           Console.WriteLine(sayi);
+        }
+        //IndexOf Metodu
+        Console.WriteLine("****** Array IndexOf ******");
+        //23 sayisin indexi kactiri getirir. 
+        Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+        //Resize Metodu
+        Console.WriteLine("****** Array Resize ******");
+        Array.Resize<int>(ref sayiDizisi, 9); // Eleman sayisini 9 yap demektir.
+        sayiDizisi[8] = 99; 
+        foreach (var sayi in sayiDizisi)
+        {
+           Console.WriteLine(sayi);
+        }
+        
+    }
+}
