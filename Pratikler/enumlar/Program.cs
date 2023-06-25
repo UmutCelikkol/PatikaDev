c213f26be91b506885481daa3e22f881bc042f42
Umut Çelikkol <umutcelikkol@yahoo.com.tr>
2023-06-25 21:07:03 +0300

    enumlar ve structlar eklendi

---
+++ Pratikler/enumlar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace enumlar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    // Uygulama geliştirirken sabit değerlerle çalışmak durumunda kalırız. Bu noktalarda okunabilirliği yüksek bir program yazmak istiyorsak enum'lardan faydalanırız.
+    // "enum" anahtar kelimesi enumeration yani numaralandırma kelimesinden gelir. Sayısal verilerı string ifadelerle saklamanızı sağlar. Okunabilirliğe katkısı da tam olarak burdan gelir diyebiliriz.
+        Console.WriteLine(Gunler.pazartesi);
+        Console.WriteLine((int)Gunler.cumartesi); // cast ettik ve 6 degeri gelir
+        int sicaklik =32;
+        if (sicaklik<=(int)HavaDurumu.Normal)
+        {
+            Console.WriteLine("Disariya cikmak icin havanin isinmasini bekleyin");
+        }else if(sicaklik>= (int)HavaDurumu.Sicak)
+        {
+             Console.WriteLine("Disariya cikmak icin cok sicak bir gun");
+        }else if(sicaklik>= (int)HavaDurumu.Normal&&sicaklik<(int)HavaDurumu.Coksicak)
+        {
+             Console.WriteLine("Hava hala sicak!");
+        }
+
+    }
+}
+enum Gunler
+{
+    // Aslinda burada numerik tutar
+    pazartesi=1,
+    sali,
+    carsamba,
+    persembe,
+    // buraya kadar 4'e sayar
+    //burada cuma"ya 23 verdigimizda ardisiklik 23'e gore artmaya baslar
+    cuma=23,
+    cumartesi,
+    pazar
+}
+enum HavaDurumu
+{
+    Soguk=5,
+    Normal=20,
+    Sicak=25,
+    Coksicak=30
+}
---
+++ Pratikler/struct-kavrami/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace struct_kavrami;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Class"lar referans tipindedir.
+        // Struct"lar ancak deger tipindedir.
+        //Yapıların özellikleri:
+       // Class lar referans tipli özellikler gösterir,        Yapılar ise değer tipli özellikler gösterirler. En        temel fark budur.
+       // Diğer struct ya da sınıflardan kalıtım almazlar.
+       // Interface'lerden kalıtım alabilirler.
+       // new anahtar sözcüğü ile nesneleri yaratılabilir.
+       // Sınıflar gibi metot, property ve field'lardan        oluşurlar.
+       // Sınıf içerisinde struct, struct içerisinde de        sınıf oluşturulabilir.
+       // Static üye barındırabilirler.
+        Dikdortgen dikdortgen = new Dikdortgen();
+        dikdortgen.KisaKenar =3;
+        dikdortgen.UzunKenar =4;
+        Console.WriteLine("Class alan hesabi :{90}",dikdortgen.AlanHesapla());
+
+        Dikdortgen_Struct dikdortgen_struct; // = new Dikdortgen_Struct(); // Goruldugu uzere instance yaratmadan ilgili metodlari cagirabiliyoruz
+
+        dikdortgen_struct.KisaKenar =3;
+        dikdortgen_struct.UzunKenar =4;
+        Console.WriteLine("Class alan hesabi :{90}", dikdortgen_struct.AlanHesapla());    
+
+    }
+}
+class Dikdortgen
+{
+    public int KisaKenar;
+    public int UzunKenar;
+
+    public Dikdortgen()
+    {
+        KisaKenar =3;
+        UzunKenar =4;
+    }
+    public long AlanHesapla()
+    {
+        return this.KisaKenar * this.UzunKenar;
+    }
+}
+struct Dikdortgen_Struct
+{
+    public int KisaKenar;
+    public int UzunKenar;
+    public Dikdortgen_Struct()
+    {
+        KisaKenar =3;
+        UzunKenar =4;
+    }
+    public long AlanHesapla()
+    {
+        return this.KisaKenar * this.UzunKenar;
+    }
+}
